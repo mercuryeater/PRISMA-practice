99f8c3519341b0585b1b7fe32c7321b3e8627b84
Federico <federicoc03@gmail.com>
2023-06-04 12:00:03 -0500

    Method GET by Id working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
@@ -8,41 +8,52 @@ app.use(express.json());
 
 const PORT = process.env.PORT ?? 3001;
 
-app.get('/api/persons', async (req, res) => {
+app.get("/api/persons", async (req, res) => {
   const persons = await prisma.persons.findMany();
 
   return res.json(persons);
-})
+});
 
-
-app.post('/api/persons', async (req, res) => {
-  try{
-  const {name, phone_number} = req.body;
-  const contactCreated = await prisma.persons.create({
+app.post("/api/persons", async (req, res) => {
+  try {
+    const { name, phone_number } = req.body;
+    const contactCreated = await prisma.persons.create({
       data: {
-          name,
-          phone_number,
-      }
-  });
-  return res.json(contactCreated);
-} catch(err){
-  console.error(err);
-}
-})
-
-
-app.get('/info', async (req, res) => {
-  try{
-  const persons = await prisma.persons.findMany();
-  const people = persons.length;
-  const currentDate = new Date()
-  return res.send(`Phonebook has info for ${people} people<br><br>Time: ${currentDate}`);
-  } catch(err){
+        name,
+        phone_number,
+      },
+    });
+    return res.json(contactCreated);
+  } catch (err) {
     console.error(err);
   }
-})
+});
 
+app.get("/info", async (req, res) => {
+  try {
+    const persons = await prisma.persons.findMany();
+    const people = persons.length;
+    const currentDate = new Date();
+    return res.send(
+      `Phonebook has info for ${people} people<br><br>Time: ${currentDate}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+});
 
+app.get("/api/persons/:id", async (req, res) => {
+  const id = req.params.id;
+  const person = await prisma.persons.findMany({
+    where: {
+      id: id,
+    },
+  });
+  if (Object.keys(person).length === 0)
+    return res.status(404).json({ message: "Person not found" });
+
+  return res.json(person);
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
@@ -53,12 +64,12 @@ app.listen(PORT, () => {
 //     handleGetDataById,
 //     handleDeleteById,
 //     handleCreateData,
-//     handleUpdateData 
+//     handleUpdateData
 // } = require("./controller");
 
 // app.use(express.json());
 
-// app.get("/api/persons", handleGetAllData); 
+// app.get("/api/persons", handleGetAllData);
 
 // app.get("/info", (req, res) => {
 //   const people = persons.length;
